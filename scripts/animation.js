69a351d0015bd42760f08a1338f5baf51a3e846d
AlessandroRomanelli <aleromrod@gmail.com>
2017-03-09 21:10:25 +0100

    Hotfix, about2 bullets, thirdSlide gradient

--- scripts/animation.js
+++ scripts/animation.js
@@ -30,34 +30,37 @@
 			clearInterval(colorGenerator);
 			document.body.style.overflowY = 'visible';
 
+			//Sticky header
 			!function stickyHeader(){
 				var didScroll;
 				var lastScrollTop = 0;
 				var delta = 5;
 				var navbarHeight = $("#nav-header").outerHeight();
 
-				$('button').on('click', function() {
-					$.smoothScroll('+=' + $(window).height());
-				});
-
+				//When user scrolls
 				$(window).scroll(function(event){
 					didScroll = true;
 				});
 
+				//Function to be called when user scrolls
 				function hasScrolled() {
+					//ScrollTop, store the distance from top of page
 					var st = $(this).scrollTop();
+					// If user scrolls less than 5 px
 					if(Math.abs(lastScrollTop - st) <= delta)
-					return;
+					//Quit
+						return;
+					// If it's scrolling up
 					if (st > lastScrollTop && st > navbarHeight){
-							// Scroll right
+							// Scroll right, Hide
 							$('#nav-header').removeClass('nav-left').addClass('nav-right');
 					} else {
-							// Scroll left
+							// Scroll left, show
 							if(st + $(window).height() < $(document).height()) {
 									$('#nav-header').css("position", "fixed");
 									$('#nav-header').removeClass('nav-right').addClass('nav-left');
-							}
-					}
+							};
+					};
 					lastScrollTop = st;
 					setTimeout(function(){
 						if (lastScrollTop !== 0) {
